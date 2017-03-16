88c84beb379674170d22b4068da85936d61c8d2d
Gus Fowler <gus.fowler12@gmail.com>
2017-03-15 22:57:40 -0500

    3.15.17 recovered corrupted files

--- core/src/com/gusfowler/twoinfinitegame/Piece.java
+++ core/src/com/gusfowler/twoinfinitegame/Piece.java
@@ -1,5 +1,32 @@
 package com.gusfowler.twoinfinitegame;
 
-public class Piece {
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
 
+public class Piece {
+	private BitmapFont font;
+	private GlyphLayout text;
+	
+	public Rectangle rec;
+	public float boardX, boardY;
+	public int scoreVal;
+	
+	public Piece(BitmapFont f, float x, float y, float width, float height, float bX, float bY) 
+	{
+		rec = new Rectangle(x, y, width, height);
+		boardX = bX;
+		boardY = bY;
+		int sV = MathUtils.random(1,2);
+		if (sV == 1) scoreVal = 2;
+		if (sV == 2) scoreVal = 4;
+		text = new GlyphLayout(font, Integer.toString(scoreVal));
+	}
+	
+	public GlyphLayout getGlyph() 
+	{
+		text.setText(font, Integer.toString(scoreVal));
+		return text;
+	}
 }
